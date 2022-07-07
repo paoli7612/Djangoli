cdeb0890072cf8484a7d31c811c4cbf50a0331fd
paoli7612 <tomaoli7612@gmail.com>
2022-07-07 15:28:38 +0200

    step 2

--- djangoli/urls.py
+++ djangoli/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drinks import views
 
 urlpatterns = [
     path('', views.drinks),
     path('admin/', admin.site.urls),
-    path('drinks/', views.drinks)
+    path('drinks/', include('drinks.urls'))
 ]
