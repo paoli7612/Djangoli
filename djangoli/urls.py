33de9a838a4ac6e46cb73dccd934d44d8ed55cd6
paoli7612 <tomaoli7612@gmail.com>
2022-07-07 13:08:17 +0200

    drink urls

--- djangoli/urls.py
+++ djangoli/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drinks import views
 
 urlpatterns = [
+    path('', views.drinks),
     path('admin/', admin.site.urls),
+    path('drinks/', views.drinks)
 ]
--- drinks/views.py
+++ drinks/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def drinks(request):
+    return render(request, 'drinks/all.html')
