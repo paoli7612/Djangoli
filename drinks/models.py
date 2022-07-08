c8e6fe810ada767b84bda045475c0ab0d48a1d22
paoli7612 <tomaoli7612@gmail.com>
2022-07-08 09:37:28 +0200

    drinks view

--- drinks/models.py
+++ drinks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Drink(models.Model):
+    nome = models.CharField(max_length=64)
+    immagine = models.ImageField()
+    prezzo = models.FloatField()
+
+    def __str__(self):
+        return self.nome
--- drinks/views.py
+++ drinks/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Drink
 
 # Create your views here.
 def drinks(request):
-    return render(request, 'drinks/all.html')
+    return render(request, 'drinks/all.html', {
+        'drinks': Drink.objects.all()
+    })
+
+def drink():
+    return render(request, 'drinks/show.html', {
+        drink: None
+    })
